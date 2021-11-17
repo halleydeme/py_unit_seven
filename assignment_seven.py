8bcb331b51f1679e0c9ef52bc41e9363ce732b69
halley <demeha@ssfs.org>
2021-11-17 09:02:08 -0500

    Unit 7 assignment

--- assignment_seven.py
+++ assignment_seven.py
@@ -14,12 +14,27 @@ def encode(phrase,shift):
     return new_phrase
 
 def decode(phrase,shift):
-
+    alpha = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    new_phrase=""
+    for letter in phrase:
+        value = alpha.index(letter)
+        value -= shift
+        new_letter = alpha[value]
+        new_phrase += new_letter
+    return new_phrase
 
 
 
 def main():
     k = input("Please enter a word or phrase you would like to have encoded or decoded")
+    s= input("By how much would you like to shift?")
+    j = input("Enter e to have your phrase encoded, d to have your phrase decoded, or q to quit.")
+    if j == "e":
+        encode(k,s)
+    if j == "d":
+        decode(k,s)
+    if j == "q":
+        return done
 
 
 if __name__ == '__main__':
